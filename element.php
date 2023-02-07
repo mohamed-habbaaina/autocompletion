18d9ff160c2a95508fdbb584eaa7d0710a98159b
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-02-07 12:04:42 +0100

    la page element.php, l'affichage de resultat de la recheche

---
+++ element.php
@@ -0,0 +1,59 @@
+<?php
+if (isset($_GET['id'])):
+    $id = $_GET['id'];
+
+    //  La connexion DB
+    try{
+    $db = new PDO("mysql:host=localhost;dbname=autocompletion", "root", "");
+    }
+    catch(PDOException $e){
+        echo 'ERROR CONNECTION: ' . $e->getMessage();
+    }
+
+    //  Query where "id"
+    $req = $db->prepare("SELECT * FROM `plages_Bouches-du-RHÔNE` WHERE id=?");
+    $req->execute(["$id"]);
+    $data = $req->fetchAll(PDO::FETCH_ASSOC);   //  Récupérer la data dans BD.
+    $nom = $data[0]['nom']; //   id
+    $adresse = $data[0]['adresse']; //  "adresse"
+    $lien = $data[0]['lien'];       //  "lien"
+else:           //  redirection vers index.php pour géré les erreurs
+    header('location: index.php');
+endif;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/element.css">
+    <title>Element</title>
+</head>
+<body>
+    <header>
+        <button><a href="./index.php">Home</a></button>
+    </header>
+    <main>
+        <div >
+            <h3>Affichage des informations :</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Plage</th>
+                        <th>Adresse</th>
+                        <th>Lien pour plus d'info</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?=$nom ?></td>
+                        <td><?=$adresse ?></td>
+                        <td><a href="<?=$lien ?>"><?=$lien ?></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+</body>
+</html>
