27471e1f26a39c861ee1f713b14097d58643a98f
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-02-07 11:59:06 +0100

    le fichier index final, ajout de l'affichage des erreurs

--- index.php
+++ index.php
@@ -9,13 +9,14 @@
 </head>
 <body>
     <main>
-        <form action="./recherche.php" id="form">
-            <label for="search">Plages</label>
-            <input type="search" name="search" id="search" placeholder="Rechercher une Plage">
-            <button type="input">Recherche</button>
-            <span id="result"></span>
-
+        <!-- Le formulaire de recherche -->
+        <form action="./element.php?id=" id="form">
+            <label for="search">Plages: </label>
+            <input type="search" name="search" id="search" placeholder="Rechercher une Plage ...">
+            <button id="btn" disabled>Recherche</button>
         </form>
+        <!-- L'affichage des resultats -->
+        <ul id="result"></ul>
     </main>
     <script src="./app.js"></script>
 </body>
