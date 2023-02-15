1f9cd4f8dcab2a7c6dadfc235c223d7a30698283
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-02-15 15:45:55 +0100

    petite modification pour margin form

--- element.php
+++ element.php
@@ -10,7 +10,28 @@ if (isset($_GET['id'])):
         echo 'ERROR CONNECTION: ' . $e->getMessage();
     }
 
+    //  //   ?connexion Plesk
+    //  $servername = "localhost";
+    //  $database = "mohamed-habbaaina_autocompletion";
+    //  $username = "modul_connexion";
+    //  $password = "autocompletion1";
+     
+    //  try {
+    //    $db = new PDO("mysql:host=$servername;dbname=$database",$username,$password);
+    //    // set the PDO error mode to exception
+    //    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //  } catch(PDOException $e) {
+    //    echo "Connection failed: " . $e->getMessage();
+    //  }
+
     //  Query where "id"
+
+            /*
+         ! pour la db : "changement du nom de la table sans accents" avant "RHÔNE" après "Rhone"
+
+             $req = $db->prepare("SELECT * FROM `plages_Bouches-du-Rhone` WHERE id=?");
+        */
+
     $req = $db->prepare("SELECT * FROM `plages_Bouches-du-RHÔNE` WHERE id=?");
     $req->execute(["$id"]);
     $data = $req->fetchAll(PDO::FETCH_ASSOC);   //  Récupérer la data dans BD.
--- recherche.php
+++ recherche.php
@@ -10,6 +10,27 @@ if (isset($_GET['search'])):
     catch(PDOException $e){
         echo 'ERROR CONNECTION: ' . $e->getMessage();
     }
+
+    // //   ?connexion Plesk
+    // $servername = "localhost";
+    // $database = "mohamed-habbaaina_autocompletion";
+    // $username = "modul_connexion";
+    // $password = "autocompletion1";
+    
+    // try {
+    //   $db = new PDO("mysql:host=$servername;dbname=$database",$username,$password);
+    //   // set the PDO error mode to exception
+    //   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // } catch(PDOException $e) {
+    //   echo "Connection failed: " . $e->getMessage();
+    // }
+
+        /*
+         ! pour la db : "changement du nom de la table sans accents" avant "RHÔNE" après "Rhone"
+
+             $req = $db->prepare("SELECT * FROM `plages_Bouches-du-Rhone` WHERE nom LIKE '%{$requ}%'");
+        */
+
     $req = $db->prepare("SELECT * FROM `plages_Bouches-du-RHÔNE` WHERE nom LIKE '%{$requ}%'");
     $req->execute();
     $data = $req->fetchAll(PDO::FETCH_ASSOC);
